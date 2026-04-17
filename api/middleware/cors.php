7fa2a65f3a8d0a47683ef31098dc439aee8c5818
joelerich08-prog <joelerich08@gmail.com>
2026-04-18 01:33:09 +0800

    Add Vercel app URL to CORS allowed origins

--- api/middleware/cors.php
+++ api/middleware/cors.php
@@ -10,6 +10,7 @@ $defaultOrigins = [
     'http://127.0.0.1:3000',
     'http://localhost:3001',
     'http://127.0.0.1:3001',
+    'https://kakai-kutkutin.vercel.app',
 ];
 
 $envOrigins = getenv('CORS_ALLOWED_ORIGINS');
